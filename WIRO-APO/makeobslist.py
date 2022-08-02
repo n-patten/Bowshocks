522a9777736aa6fb889ab7220672ecf9a7d7f0f7
Nikhil Patten <nikhil@ant.uwyo.edu>
2022-08-01 20:00:09 -0600

    Made some changes

--- WIRO-APO/makeobslist.py
+++ WIRO-APO/makeobslist.py
@@ -1,5 +1,8 @@
 import argparse
 import numpy as np
+import astropy as ap
+from astropy.time import Time
+from astropy.coordinates import get_sun
 # NP Necessary imports
 
 def names(wiro):
@@ -38,22 +41,83 @@ def dec(wiro):
 	# NP Reading in dec column
 	return dec
 
+def Gmags(wiro):
+	'''Extracts objects' gmags from a list of objects in WIRO format.
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Inputs
+	-wiro: ndarray. A table containing objects in WIRO format.
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Outputs
+	-gmags: ndarray. An array of object gmags'''
+	gmags = wiro[:,8]
+	# NP Reading in dec column
+	return gmags
+
+def limitobjs(wiro, glim):
+	'''Extracts objects' decs from a list of objects in WIRO format.
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Inputs
+	-wiro: ndarray. A table containing objects in WIRO format.
+	-glim: int. The lower g-magnitude to filter by.
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Outputs
+	-ii: ndarray. A boolean array corresponding to which objects
+	satisfy the given constraints.'''
+	sunra = get_sun(Time.now()).ra.hour
+	midnight = np.mod(12 +sunra, 24)
+	rarange = 3
+	raupper = midnight +rarange
+	ralower = midnight -rarange
+	print("Sun: " +str(sunra) +" hours")
+	print("Midnight: " +str(midnight) +" hours")
+	print("RA upper: " +str(raupper) +" hours")
+	print("RA lower: " +str(ralower) +" hours")
+	G = np.array([wiro[:,8][i][2:] for i in range(len(wiro[:,8]))]\
+		, dtype = float)
+	decdeg = np.array([wiro[:,3][i][:3] for i in range(len(wiro))]\
+		, dtype = float)
+	rahour = np.array([wiro[:,2][i][:2] for i in range(len(wiro))]\
+		, dtype = float)
+	if raupper > 24:
+		a = np.mod(raupper, 24)
+		b = ralower
+		print(str(a) +" " +str(b))
+		ii = (G > glim) & (decdeg > -45) & (rahour <= a)\
+			| (rahour >= b)
+	elif ralower < 0:
+		a = raupper
+		b = 24 +ralower
+		print(str(a) +" " +str(b))
+		ii = (G > glim) & (decdeg > -45) & (rahour >= b)\
+		| (rahour <= a)
+	else:
+		ii = (G > glim) & (decdeg > -45) & (rahour <= raupper)\
+			& (rahour >= ralower)
+	return ii
+
 if(__name__ == '__main__'):
 	parser = argparse.ArgumentParser(description = 'Module to write\
 		a list of targets in APO format from a list of targets\
 		in WIRO format. APO format example can be found in\
 		/d/zem1/hak/chip/observing/APO/ZetaOphStars.txt.\
 		WIRO format example can be found in\
-		/d/zem1/hak/chip/Nikhil/Nikhiltargets.cat.')
-	parser.add_argument('path', type = str, help = 'Path to WIRO file.\
-		Example: /d/...')
+		/d/zem1/hak/chip/Nikhil/Nikhiltargets.cat. Additionally,\
+		limit selection by RA and Dec bounds and G magnitude.\
+		\
+		KOSMOS has an airmass limit of ')
+	parser.add_argument('filename', type = str, help = 'Name of WIRO file.\
+		Example: blah.cat')
 	# NP Defining parser path argument
 	parser.add_argument('name', type = str, help = \
 		'Name of generated file. Example: name')
-	# NP Defining parser new file name argument
+	# NP Defining parser name argument
+	parser.add_argument('gmag', type = float, help = \
+		'G Magnitude ceiling. Selection will exclude objects'
+			' brighter than this constraint. Example: 12')
+	# NP Setting g magnitude limit
 	args = parser.parse_args()
 	# NP Adding parser to run at command line
-	wirofile = args.path
+	wirofile = args.filename
 	# NP Defining WIRO list
 	wiro = np.loadtxt(wirofile , dtype = str, delimiter = ' ')
 	# NP Reading in WIRO list
@@ -63,11 +127,16 @@ if(__name__ == '__main__'):
 	# NP Extracting object ras
 	decs = dec(wiro)
 	# NP Extracting objects decs
+	gs = Gmags(wiro)
+	# NP Extracting objects gmags
 	APO = [n[i] +' ' +ras[i] +' ' +decs[i] +\
-		' RotType=Horizon; RotAng=90' for i in range(len(n))]
+		' RotType=Horizon; RotAng=90 #' +gs[i]\
+		for i in range(len(n))]
 	# NP Generating array of objects in APO format
-	np.savetxt('/d/users/nikhil/observing/' +args.name +'.txt', APO,\
-		fmt = '%s')
+	index = limitobjs(wiro, args.gmag)
+	dimmerAPO = np.array(APO)[index]
+	np.savetxt(args.name +'.txt', APO, fmt = '%s')
+	np.savetxt(args.name +'dim.txt', dimmerAPO, fmt = '%s')
 	# NP Writing APO objects to a file with new inputted file name
 	print("Done.")
 	# NP Printing message when conversion is complete
